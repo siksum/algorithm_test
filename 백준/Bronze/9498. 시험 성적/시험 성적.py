8df8d1ee2415f8d4c037c167cd54b9bbc29128b9
sikk <88465382+siksum@users.noreply.github.com>
2024-10-17 16:47:38 +0900

    [Bronze V] Title: 시험 성적, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,20 @@
+def compare_score(score):
+    if 90 <= score <= 100:
+        print('A')
+    elif 80 <= score < 90:
+        print('B')
+    elif 70 <= score < 80:
+        print('C')
+    elif 60 <= score < 70:
+        print('D')
+    else:
+        print('F')
+        
+        
+def main():
+    score = int(input())
+    compare_score(score)
+    
+    
+if __name__ == '__main__':
+    main()
