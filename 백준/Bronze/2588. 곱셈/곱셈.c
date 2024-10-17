f9ec3047f415db0150b0930b72babf0935eede32
sikk <88465382+siksum@users.noreply.github.com>
2024-10-17 16:31:33 +0900

    [Bronze III] Title: 곱셈, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+	int A;
+	int B;
+	scanf("%d %d", &A, &B);
+
+	printf("%d\n", A*(B%10));
+	printf("%d\n", A*((B/10)%10));
+	printf("%d\n", A*((B/100)%10));
+	printf("%d", A*B);
+
+	return 0;
+}
