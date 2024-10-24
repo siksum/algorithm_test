a9c77a61257788b93db709aba1371fd8fd595936
sikk <88465382+siksum@users.noreply.github.com>
2024-10-24 16:23:56 +0900

    [Bronze III] Title: 약수 구하기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.py
@@ -0,0 +1,18 @@
+def main():
+    
+    N, K = map(int, input().split())
+    
+    arr = []
+    
+    for i in range(1, N+1):
+        if N % i == 0:
+            arr.append(i)
+    
+    if len(arr) < K :
+        print(0)
+    else:
+        print(arr[K-1])   
+    
+
+if __name__ == '__main__':
+    main()
